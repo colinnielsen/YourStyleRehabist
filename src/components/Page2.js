ae0e2407fca42aac5a5a505b48d586fc5e4444e6
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2020-05-18 15:56:02 -0600

    Page2 made responsive

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 import SplashImage from './Page2SplashImage';
 
 const Page2Container = styled.section`
+   @media only screen and (max-width: 800px) {
+      transform: none;
+      flex-flow: column wrap;
+   }
    height: 110vh;
    width: 100%;
    transform: translate(-2px);
@@ -17,11 +21,22 @@ const ContentContainer = styled.div`
    width: 100%;
    height: 90%;
    display: flex;
+   @media only screen and (max-width: 800px) {
+      transform: none;
+      flex-flow: column wrap;
+   }
    align-items: center;
    justify-content: center;
 `;
 
 const ImageContainer = styled.div`
+   @media only screen and (max-width: 800px) {
+      transform: none;
+      display: flex;
+      align-items: top;
+      justify-content: center;
+      width: 100%;
+   }
    z-index: -1;
    transform: translate(10%);
 `;
@@ -38,6 +53,14 @@ const Gradient = styled.div`
 `;
 
 const SplashBox = styled.div`
+   @media only screen and (max-width: 800px) {
+      width: 250px;
+      height: 80px;
+      font-style: normal;
+      font-size: 40px;
+      margin: 20px;
+      top: 120vh;
+   }
    position: absolute;  
    margin: 40px;
    z-index: 2;
@@ -57,6 +80,13 @@ const SplashBox = styled.div`
 `;
 
 const SplashColorBox = styled.div`
+   @media only screen and (max-width: 800px) {
+      transform: none;
+      width: 85%;
+      position: absolute;
+      padding: 15px;
+      height: 450px;
+   }
    background: #C5B9AD;
    z-index: 2;
    width: 500px;
@@ -66,6 +96,10 @@ const SplashColorBox = styled.div`
 `;
 
 const SplashTextBox = styled.div`
+   @media only screen and (max-width: 800px) {
+      font-size: 30px;
+      line-height: 35px;
+   }
    font-family: Inria;
    font-size: 46px;
    line-height: 55px;
@@ -75,13 +109,22 @@ const SplashTextBox = styled.div`
 `;
 
 const Line = styled.div`
+   @media only screen and (max-width: 800px) {
+      margin: 0;
+   }
    height: 3px;
-   width: 100%;
+   width: 90%;
    border: 2px solid #575757;
    margin-top: 8px;
 `;
 
 const ExplainationText = styled.div`
+   @media only screen and (max-width: 800px) {
+      margin: 0;
+      font-size: 20px;
+      line-height: 28px;
+      margin-top: 20px;
+   }
    margin-top: 25px;
    font-family: Inria;
    font-size: 29px;
@@ -94,9 +137,9 @@ const Page2 = () =>
    <Page2Container>
       <ContentContainer>
          <ImageContainer>
-         <SplashBox>
-            What I <br />do for you -
-         </SplashBox>
+            <SplashBox>
+               What I do for you -
+            </SplashBox>
             <Gradient />
             <SplashImage />
          </ImageContainer>

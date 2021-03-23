b22d36348471e97be307bf945c5bee94e785c613
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2021-03-22 20:03:53 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,28 +9,28 @@ import Slide4 from '../components/Slide4';
 import Slide5 from '../components/Slide5';
 import "../styles/default.css"
 
-export default () =>
-  <>
+export default () => {
+ React.useEffect(() => {
+    (function(w, d, t, s, n) {
+        w.FlodeskObject = n;
+        var fn = function() {
+          (w[n].q = w[n].q || []).push(arguments);
+        };
+        w[n] = w[n] || fn;
+        var f = d.getElementsByTagName(t)[0];
+        var e = d.createElement(t);
+        var h = '?v=' + new Date().getTime();
+        e.async = true;
+        e.src = s + h;
+        f.parentNode.insertBefore(e, f);
+      })(window, document, 'script', 'https://assets.flodesk.com/universal.js', 'fd');
+      window.fd('form', {
+        formId: '60510560568cfe0ea1997cad'
+      });
+ },[])
+ return <>
     <Helmet>
       <link rel="icon" href={favicon} />
-        <script>
-  (function(w, d, t, s, n) {
-    w.FlodeskObject = n;
-    var fn = function() {
-      (w[n].q = w[n].q || []).push(arguments);
-    };
-    w[n] = w[n] || fn;
-    var f = d.getElementsByTagName(t)[0];
-    var e = d.createElement(t);
-    var h = '?v=' + new Date().getTime();
-    e.async = true;
-    e.src = s + h;
-    f.parentNode.insertBefore(e, f);
-  })(window, document, 'script', 'https://assets.flodesk.com/universal.js', 'fd');
-  window.fd('form', {
-    formId: '60510560568cfe0ea1997cad'
-  });
-</script>
     </Helmet>
     <SEO />
     <Slide1 />
@@ -39,3 +39,4 @@ export default () =>
     <Slide4 />
     <Slide5 />
   </>;
+}
